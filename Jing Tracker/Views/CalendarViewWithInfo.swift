879217cd11083ac9922dee25efb1dcfdf9353057
Joshua Jay Runyan <runyan@ashtacore.com>
2026-01-09 00:46:06 -0600

    Calendar can now be used to edit count of each day

--- Jing Tracker/Views/CalendarViewWithInfo.swift
+++ Jing Tracker/Views/CalendarViewWithInfo.swift
@@ -3,11 +3,21 @@ import SwiftUICalendar
 import SwiftData
 
 struct CalendarViewWithInfo: View {
+    @Environment(\.modelContext) private var modelContext
     @Query(sort: \WellnessEvent.date, order: .forward) private var events: [WellnessEvent]
     
     @ObservedObject var controller = CalendarController()
     @State var focusDate: YearMonthDay? = nil
-    @State var focusInfo: [EventType]? = nil
+    
+    var focusEvents: [EventType] {
+        guard let focusDate else { return [] }
+        let calendar = Calendar.current
+        
+        return events.filter {
+            let components = calendar.dateComponents([.year, .month, .day], from: $0.date)
+            return YearMonthDay(year: components.year!, month: components.month!, day: components.day!) == focusDate
+        }.map { $0.type }
+    }
     
     private var informations: [YearMonthDay: [EventType]] {
         var result = [YearMonthDay: [EventType]]()
@@ -28,7 +38,25 @@ struct CalendarViewWithInfo: View {
         
         return result
     }
-
+    
+    func countFor(event: EventType) -> Int {
+        focusEvents.filter { $0 == event }.count
+    }
+    
+    func logEvent(type: EventType) {
+        guard focusDate != nil else { return }
+        let newEvent = WellnessEvent(type: type, date: focusDate!.date!)
+        modelContext.insert(newEvent)
+    }
+    
+    func removeEvent() {
+        guard focusDate != nil else { return }
+        let eventToRemove = events.first(where: { $0.date == focusDate!.date })
+        guard eventToRemove != nil else { return }
+        
+        modelContext.delete(eventToRemove!)
+    }
+    
     var body: some View {
         GeometryReader { reader in
             VStack {
@@ -67,7 +95,7 @@ struct CalendarViewWithInfo: View {
                                     let eventCount = daysEvents.count { $0 == event }
                                     if eventCount > 0 {
                                         let color = event.color.opacity(0.75)
-                                        if focusInfo != nil {
+                                        if focusDate != nil {
                                             Rectangle()
                                                 .fill(color)
                                                 .frame(width: geometry.size.width, height: 4, alignment: .center)
@@ -96,25 +124,44 @@ struct CalendarViewWithInfo: View {
                             withAnimation {
                                 if focusDate == date {
                                     focusDate = nil
-                                    focusInfo = nil
                                 } else {
                                     focusDate = date
-                                    focusInfo = informations[date]
                                 }
                             }
                         }
                     }
                 })
-                if let daysEvents = focusInfo {
+                if focusDate != nil {
                     List(EventType.allCases) { event in
-                        let eventCount = daysEvents.count { $0 == event }
+                        let eventCount = countFor(event: event)
                         let color = event.color.opacity(0.75)
+                        
                         HStack(alignment: .center, spacing: 0) {
                             Circle()
                                 .fill(color)
                                 .frame(width: 12, height: 12)
-                            Text("\(event.title) x\(eventCount)")
+                            Text("\(event.title)")
                                 .padding(.leading, 8)
+                            
+                            Spacer()
+                            
+                            Button(action: { logEvent(type: event) }) {
+                                Label("", systemImage: "plus")
+                                    .labelStyle(.iconOnly)
+                                    .foregroundStyle(.green)
+                            }
+                            .buttonStyle(.borderless)
+                            
+                            Text("x\(eventCount)")
+                                .padding(8)
+                                .frame(minWidth: 40)
+                            
+                            Button(action: removeEvent) {
+                                Label("", systemImage: "minus")
+                                    .labelStyle(.iconOnly)
+                                    .foregroundStyle(.red)
+                            }
+                            .buttonStyle(.borderless)
                         }
                     }
                     .frame(width: reader.size.width, height: 160, alignment: .center)
